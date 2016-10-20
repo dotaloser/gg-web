7e88081d90d4d2e379562934dee04c2482f17e98
dennis <waywaitway@163.com>
2016-10-20 16:33:23 +0800

    idea page init

---
+++ src/main/java/com/dota/gg/idea/IdeaControl.java
@@ -0,0 +1,19 @@
+package com.dota.gg.idea;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+   * 描述：
+   * @author: ning.li
+   * @version: 2016年10月20日 下午3:25:49
+ */
+@Controller
+@RequestMapping(value = "/idea")
+public class IdeaControl {
+	
+	@RequestMapping(value = "/dennis")
+	public String gg(){
+		return "idea/dennis";
+	}
+}
